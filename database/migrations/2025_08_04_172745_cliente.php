b489f9bf006774dc0a633e243ac31fa089397c6d
juaniraser <raiser@MacBook-Air-de-Juan.local>
2025-08-04 21:41:15 -0500

    login ok

---
+++ database/migrations/2025_08_04_172745_cliente.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cliente', function (blueprint $table) {
+            $table->id('id_cliente');//DNI
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->string('cupon');
+            $table->timestamps();
+
+            $table->foreignId('id_moto')->references('id_moto')->on('moto')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cliente');
+    }
+};
---
+++ database/migrations/2025_08_04_172750_voucher.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voucher', function (blueprint $table) {
+            $table->id('id_voucher');
+            $table->decimal('precio_final', 5, 2);
+            $table->timestamps();
+
+            $table->foreignId('id_cliente')->references('id_cliente')->on('cliente')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreignId('id_moto')->references('id_moto')->on('moto')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voucher');
+    }
+};
